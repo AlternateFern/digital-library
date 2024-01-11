d800a0a1b65c1ac5b74c8efcdc84c0e47ab6531a
AlternateFern <ayatabg2@gmail.com>
2024-01-11 09:41:56 +0700

    navbar rework

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,22 +1,8 @@
-<ul class="navbar nav nav-underline" style="background-color: rgba(57, 33, 19, 0.6);">
-    <div class="container-fluid">
-      <li class="nav-item">
-        <a class="navbar-brand" href="{{ url('home') }}" style="font-size:30px; font-weight:bold; color:white;"">DL</a>
-        <a class="brand-subtitle" href="{{ url('home') }}" style="text-decoration: none; font-weight:bold; color:white;"> => Digital Library </a>
-      </li>
-      <div class="d-flex">
-        <li class="nav-item" style="margin-right: 30px;">
-            <a class="nav-link" href="{{url('home')}}" style="color:white;">Home</a>
-          </li>
-        <li class="nav-item" style="margin-right: 30px;">
-            <a class="nav-link" href="{{url('about')}}" style="color:white;">About</a>
-          </li>
-          <li class="nav-item" style="margin-right: 30px;">
-            <a class="nav-link" href="{{url('library')}}" style="color:white;">Library</a>
-          </li>
-        <li class="nav-item" style="margin-right: 30px;">
-          <a class="nav-link" href="{{url('logout')}}" style="color:white;">Logout</a>
-        </li>
-      </div>
-    </div>
-  </ul>
+<link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+<nav>
+  <a href="{{ url('home') }}">Home</a>
+  <a href="{{ url('library') }}">Library</a>
+  <a href="{{ url('home') }}">Trending</a>
+  <a href="{{ url('home') }}">About</a>
+  <a id="logout" href="{{ url('logout') }}">Logout</a>
+</nav>
